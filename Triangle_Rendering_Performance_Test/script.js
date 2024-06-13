25db62396d47e501c3e289d85063c6054b5a8a20
akhelasi <135386105+akhelasi@users.noreply.github.com>
2024-06-13 22:35:53 +0400

    Update script.js

--- Triangle_Rendering_Performance_Test/script.js
+++ Triangle_Rendering_Performance_Test/script.js
@@ -1,4 +1,4 @@
-let numShapes = 5000; // How many figures (Shapes)
+let numShapes = 500; // How many figures (Shapes, Geometries)
 let numTrianglesInShape = 200; // How many "triangles" in one figure (Shape)
 // "triangles" რაოდენობა = numShapes * numTrianglesInShape
 
